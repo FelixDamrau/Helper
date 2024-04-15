8bde4d0d009396b3c381c3aaca0c426d77c2ecdb
Felix Damrau <mail@felixdamrau.de>
2024-04-15 18:57:08 +0200

    Make PackageData equatable - consider only Id & Version

--- src/Helper/Modules/DependencyCheckResolver.cs
+++ src/Helper/Modules/DependencyCheckResolver.cs
@@ -56,5 +56,22 @@ public partial class DependencyCheckResolver
         return table;
     }
 
-    private record PackageData(string Project, string Id, string Version, string AdditionalInfo);
+    private class PackageData(string project, string id, string version, string additionalInfo) : IEquatable<PackageData> 
+    {
+        public string Project { get; } = project;
+        public string Id { get; } = id;
+        public string Version { get; } = version;
+        public string AdditionalInfo { get; } = additionalInfo;
+
+        public bool Equals(PackageData? other)
+        {
+            return other is not null
+                && Id == other.Id 
+                && Version == other.Version;
+        }
+
+        public override bool Equals(object? obj) => obj is PackageData packageData && Equals(packageData);
+
+        public override int GetHashCode() => HashCode.Combine(Id, Version);
+    }
 }
