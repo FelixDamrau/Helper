4c286f237d600f461f15f691395a62be1d7e99e4
Felix Damrau <mail@felixdamrau.de>
2024-04-20 15:06:38 +0200

    Add model classes for problem/conflict details

---
+++ src/Helper/Model/Dependencies/PackageData.cs
@@ -0,0 +1,20 @@
+﻿namespace Develix.Helper.Model.Dependencies;
+
+public class PackageData(string project, string id, string version, string additionalInfo) : IEquatable<PackageData>
+{
+    public string Project { get; } = project;
+    public string Id { get; } = id;
+    public string Version { get; } = version;
+    public string AdditionalInfo { get; } = additionalInfo;
+
+    public bool Equals(PackageData? other)
+    {
+        return other is not null
+            && Id == other.Id
+            && Version == other.Version;
+    }
+
+    public override bool Equals(object? obj) => obj is PackageData packageData && Equals(packageData);
+
+    public override int GetHashCode() => HashCode.Combine(Id, Version);
+}
